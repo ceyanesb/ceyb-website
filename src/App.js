e93df7eef5cc5b5d9808e520d3dfa0a4799681ac
Carlos Yanes <ceyanesb@gmail.com>
2020-05-07 01:42:25 -0400

    contact firebase function added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ContactForm from './contact'
+import { FaInstagram, FaLinkedin, FaHeart } from "react-icons/fa";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p style={{fontFamily:'Raleway'}}>
           Edit <code>src/App.js</code> and save to reload. This will be my website
         </p>
         <a
@@ -18,7 +19,27 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      {/* <p>THIS WILL BE MY WEBSITE</p> */}
+      <div className="Title">Carlos Yanes</div>
+      <hr style={{width:'10%'}}></hr>
+      <div className="Subtitle">coming soon</div>
+      <div className="Develop">
+        <div className="Items">Web Apps</div>
+        <div className="Items">Mobile Apps</div>
+        <div className="Items">E-Commerce</div>
+        <div className="Items">Landing Pages</div>
+        <div className="Items">Consulting</div>
+      </div>
+      <div className="Contact">CONTACT</div>
+      <ContactForm />
+      <div className="Footer">
+        {/* <FontAwesomeIcon icon="coffee" />a */}
+        <FaInstagram/>
+        <FaLinkedin/>
+        <FaHeart/>
+      </div>
+
     </div>
   );
 }
